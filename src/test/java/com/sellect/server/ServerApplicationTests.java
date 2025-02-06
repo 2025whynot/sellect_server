bd4b7b1146d444fd80aa0ab7901de6ffd9a14e5b
sunju <iseonju745@gmail.com>
2025-02-06 17:43:01 +0900

    init: 초기 세팅(의존성, gitignore, pr과 이슈 템플릿)

---
+++ src/test/java/com/sellect/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellect.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
