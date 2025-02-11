324ce7ad6f8d15ca66e16ed9e6bd6fc760ff2b98
sunju <iseonju745@gmail.com>
2025-02-11 16:39:10 +0900

    chore: errorCode 수정(feat.이선주 버전)

--- src/main/java/com/sellect/server/tmp/TmpController.java
+++ src/main/java/com/sellect/server/tmp/TmpController.java
@@ -1,11 +1,14 @@
 package com.sellect.server.tmp;
 
-import com.sellect.server.common.exception.CommonException;
-import com.sellect.server.common.exception.enums.BError;
-import com.sellect.server.common.response.ApiResponse;
+import com.sellect.server.common.exception.ApiResponse;
+import com.sellect.server.common.exception.GlobalException;
+import com.sellect.server.common.exception.ResponseStatus;
+import com.sellect.server.tmp.controller.response.TmpRequest;
 import com.sellect.server.tmp.controller.response.TmpResponse;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,11 +29,24 @@ public class TmpController {
 
         TmpResponse tmpResponse = new TmpResponse(1L);
 
-        CommonException member = new CommonException(BError.NOT_REGISTERED, "Member");
+        GlobalException member = new GlobalException(ResponseStatus.TOKEN_NOT_VALID);
 
         if (true) {
             throw member;
         }
+        // service 에서 사용 예시
+//        Option option = optionRepository.findById(cartOptionModifyDto.getOptionId())
+//                .orElseThrow(() -> new GlobalException(ResponseStatus.NO_EXIST_OPTION));
         return ApiResponse.OK(tmpResponse);
     }
+
+    @PostMapping("/field-error")
+    public ApiResponse<TmpResponse> registerProduct2(Long userId, @Valid @RequestBody TmpRequest request) {
+
+        TmpRequest tmpRequest = new TmpRequest(request.name(), request.name2(), request.name3());
+        TmpResponse tmpResponse = new TmpResponse(1L);
+
+        return ApiResponse.OK(tmpResponse);
+    }
+
 }
