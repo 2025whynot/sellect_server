2ee73420f2960bf9c15cdc1e9eaa3fbda61df33b
kongji <kongji4092@gmail.com>
2025-02-13 17:37:54 +0900

    refactor: seller 엔티티 도메인 삭제

--- src/main/java/com/sellect/server/auth/repository/seller/SellerJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sellect.server.auth.repository.seller;
-
-import com.sellect.server.auth.repository.entity.SellerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SellerJpaRepository extends JpaRepository<SellerEntity, Long> {
-}
