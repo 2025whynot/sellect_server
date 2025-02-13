22bb949d6e0198a38699f1c62d59792bba280bdd
kongji <kongji4092@gmail.com>
2025-02-13 13:20:54 +0900

    refactor: extractToken() 관심사 이동

--- src/main/java/com/sellect/server/common/infrastructure/jwt/JwtFilter.java
+++ src/main/java/com/sellect/server/common/infrastructure/jwt/JwtFilter.java
@@ -8,6 +8,9 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,23 +21,22 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class  JwtFilter extends OncePerRequestFilter {
+public class JwtFilter extends OncePerRequestFilter {
+
     private final JwtUtil jwtUtil;
     private final UserRepository userRepository;
     private final SellerRepository sellerRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String token = extractToken(request);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtUtil.extractToken(request);
         if (token != null && jwtUtil.isTokenValid(token)) {
+
             String uuid = jwtUtil.extractUuid(token);
             String role = jwtUtil.extractRole(token);
 
@@ -55,8 +57,10 @@ public class  JwtFilter extends OncePerRequestFilter {
             }
 
             if (userDetails != null) {
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    userDetails, null, authorities);
+                authentication.setDetails(
+                    new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }
@@ -64,13 +68,6 @@ public class  JwtFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    private String extractToken(HttpServletRequest request) {
-        String authHeader = request.getHeader("Authorization");
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            return authHeader.substring(7);
-        }
 
-        return null;
-    }
 }
 
--- src/main/java/com/sellect/server/common/infrastructure/jwt/JwtUtil.java
+++ src/main/java/com/sellect/server/common/infrastructure/jwt/JwtUtil.java
@@ -4,14 +4,15 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Date;
+import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
-import java.util.Date;
-
 @Component
 public class JwtUtil {
+
     private static final Long ACCESS_TOKEN_EXPIRATION_TIME = 15 * 60 * 1000L;
     private final SecretKey secretKey;
 
@@ -22,12 +23,12 @@ public class JwtUtil {
 
     public String generateAccessToken(String uuid, String role) {
         return Jwts.builder()
-                .setSubject(uuid)
-                .claim("role", role)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRATION_TIME))
-                .signWith(secretKey, SignatureAlgorithm.HS256)
-                .compact();
+            .setSubject(uuid)
+            .claim("role", role)
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRATION_TIME))
+            .signWith(secretKey, SignatureAlgorithm.HS256)
+            .compact();
     }
 
 
@@ -50,10 +51,19 @@ public class JwtUtil {
 
     public Claims extractClaims(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(secretKey)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+            .setSigningKey(secretKey)
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+    }
+
+    public String extractToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+
+        return null;
     }
 
 
