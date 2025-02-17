c3970afaafe5790efc9266e718722f4e5e5b1633
Sangkwon Lee <sangkwon26@gmail.com>
2025-02-18 01:32:04 +0900

    style: 사용되지 않는 import문 제거 및 불필요한 세미클론 제거

--- src/main/java/com/sellect/server/category/repository/CategoryRepository.java
+++ src/main/java/com/sellect/server/category/repository/CategoryRepository.java
@@ -1,7 +1,6 @@
 package com.sellect.server.category.repository;
 
 import com.sellect.server.category.domain.Category;
-import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository {
--- src/main/java/com/sellect/server/search/domain/SearchSortType.java
+++ src/main/java/com/sellect/server/search/domain/SearchSortType.java
@@ -3,5 +3,5 @@ package com.sellect.server.search.domain;
 public enum SearchSortType {
     LATEST,   // 최신순
     PRICE_ASC,  // 가격 낮은 순
-    PRICE_DESC; // 가격 높은 순
+    PRICE_DESC // 가격 높은 순
 }
