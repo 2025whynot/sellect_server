c946758ecc74cdcb7696bff82048012227492307
Sangkwon Lee <sangkwon26@gmail.com>
2025-02-18 01:30:31 +0900

    feat: 상품 검색 기능 구현
    - 카테고리명 -> 브랜드명 -> 상품명 순으로 우선순위 검색 및 정렬
    - 검색 시에 로그 데이터 저장을 위한 이벤트 발생 및 이벤트 리스너를 통해 DB에 저장

--- src/main/java/com/sellect/server/category/repository/CategoryRepository.java
+++ src/main/java/com/sellect/server/category/repository/CategoryRepository.java
@@ -7,10 +7,4 @@ import java.util.Optional;
 public interface CategoryRepository {
 
     Optional<Category> findById(Long categoryId);
-
-    boolean existsByName(String name);
-
-    List<Category> findContainingName(String keyword);
-
-    Category findByName(String name);
 }
