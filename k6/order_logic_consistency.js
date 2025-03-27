f57190a9680f7ac357c2d71306ef6b441ed4ff4a
Sangkwon Lee <sangkwon26@gmail.com>
2025-03-28 03:28:22 +0900

    test: 기존 Fake 결제 서버 속도 문제 및 ApprovePayment에 특화된 테스트 환경을 조성하기 위해 성능 테스트 관련 코드 수정

--- k6/order_logic_consistency.js
+++ k6/order_logic_consistency.js
@@ -2,8 +2,9 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  vus: 1, // 단일 유저로 순차적 실행
-  iterations: 100, // 총 1000번 요청
+  vus: 150, // 유저 몇 명
+  iterations: 5000, // 총 요청
+  // duration: '30s', // 테스트 기간
 };
 
 const BASE_URL = 'http://localhost:8080'; // 실제 API 베이스 URL로 변경 필요
@@ -41,8 +42,8 @@ export default function () {
     return; // 주문 실패 시 종료
   }
 
-  const response = orderResponse.json();
-  const orderId = BigInt(response.result.order_id); // 수정된 부분: result.order_id로 접근
+  const response1 = orderResponse.json();
+  const orderId = BigInt(response1.result.order_id); // 수정된 부분: result.order_id로 접근
   console.log(`Order created - Order ID: ${orderId}`);
 
   // 결제 API 호출 전 로그
@@ -55,6 +56,10 @@ export default function () {
       }
   );
 
+  const response2 = paymentResponse.json();
+  const pid = BigInt(response2.result); // 수정된 부분: result.order_id로 접근
+  console.log(`Order created - Order ID: ${orderId}`);
+
   const paymentCheck = check(paymentResponse, {
     'payment ready': (r) => r.status === 200,
   });
@@ -65,5 +70,23 @@ export default function () {
     console.log(`Payment succeeded - Status: ${paymentResponse.status}, Body: ${paymentResponse.body}`);
   }
 
+
+  const approveResponse = http.post(
+      `${BASE_URL}/api/v1/test/order/payment/in-progress/${pid}`,
+      null,
+      {
+        headers: { 'Content-Type': 'application/json' },
+      }
+  );
+
+  const approveCheck = check(approveResponse, {
+    'payment approve': (r) => r.status === 200,
+  });
+
+  if (!approveCheck) {
+    console.log(`Payment failed - Status: ${approveResponse.status}, Body: ${approveResponse.body}`);
+  } else {
+    console.log(`Payment succeeded - Status: ${approveResponse.status}, Body: ${approveResponse.body}`);
+  }
 
 }
--- src/main/java/com/sellect/server/order/controller/v1/OrderV1ScenarioTestController.java
+++ src/main/java/com/sellect/server/order/controller/v1/OrderV1ScenarioTestController.java
@@ -3,8 +3,8 @@ package com.sellect.server.order.controller.v1;
 import com.sellect.server.auth.domain.User;
 import com.sellect.server.common.response.ApiResponse;
 import com.sellect.server.order.Infrastructure.port.FakePayClient;
-import com.sellect.server.order.Infrastructure.response.KakaoPayReadyResponse;
 import com.sellect.server.order.application.OrderService;
+import com.sellect.server.order.application.test.OrderServiceTestPreparePayment;
 import com.sellect.server.order.application.v1.OrderServiceV1After;
 import com.sellect.server.order.controller.request.OrderAddRequest;
 import com.sellect.server.order.controller.response.PendingOrderRegisterResponse;
@@ -29,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderV1ScenarioTestController {
 
+    private final OrderServiceTestPreparePayment OrderServiceTestPreparePayment;
     private final OrderServiceV1After orderService;
     private final OrderService orignalOrderService;
 
@@ -48,26 +49,36 @@ public class OrderV1ScenarioTestController {
 
 
     /***
-     * 결제요청 테스트용 api
+     * orderId, userId
      * */
     @PostMapping("/order/payment/{orderId}/ready/{userId}")
-    public ApiResponse<String> readyPaymentTest(@PathVariable String orderId,
-        @PathVariable Long userId) {
+    public ApiResponse<String> readyPaymentTest(@PathVariable Long orderId, @PathVariable Long userId) {
 
         log.info("[V1] ready!!");
         User user = User.builder()
             .id(userId)
             .build();
 
-        KakaoPayReadyResponse kakaoPayReadyResponse = orderService.preparePayment(user,
-            Long.valueOf(orderId));
+        // before
+        // KakaoPayReadyResponse kakaoPayReadyResponse = orderService.preparePayment(user,
+        //    Long.valueOf(orderId));
         // [TODO: 성능 테스트를 위해서 어쩔 수 없이 추가했어야 함. 배포시 삭제해야함]
-        FakePayClient.triggerInProgress(kakaoPayReadyResponse.tid(), kakaoPayReadyResponse.next_redirect_pc_url());
-        return ApiResponse.ok(kakaoPayReadyResponse.next_redirect_pc_url());
-//        String pid = orderService.preparePayment(user, orderId);
-//        return ApiResponse.ok(pid);
+        // FakePayClient.triggerInProgress(kakaoPayReadyResponse.tid(), kakaoPayReadyResponse.next_redirect_pc_url());
+        // return ApiResponse.ok(kakaoPayReadyResponse.next_redirect_pc_url());
+
+        // after
+        long result = OrderServiceTestPreparePayment.preparePayment(user, orderId);
+        // String 으로  pid 반환
+        return ApiResponse.ok(String.valueOf(result));
     }
 
+
+    @PostMapping("/order/payment/in-progress/{pid}")
+    public ApiResponse<Boolean> inProgressPaymentTest(@PathVariable Long pid) {
+
+        FakePayClient.triggerInProgressTest(pid);
+        return ApiResponse.ok(true);
+    }
 
     /***
      * 결제승인 테스트용 API
@@ -75,7 +86,7 @@ public class OrderV1ScenarioTestController {
     @GetMapping("/kakao-pay/success/{pid}")
     public ApiResponse<String> approvePayment(
         @PathVariable String pid,
-        @RequestParam("pg_token") String token) {
+        @RequestParam(value = "pg_token", defaultValue = "test") String token) {
 
         long threadId = Thread.currentThread().getId();
         log.info("{} - [V1] approve", threadId);
@@ -84,3 +95,92 @@ public class OrderV1ScenarioTestController {
         return ApiResponse.ok(pid + "success");
     }
 }
+
+
+// 백업용
+//package com.sellect.server.order.controller.v1;
+//
+//import com.sellect.server.auth.domain.User;
+//import com.sellect.server.common.response.ApiResponse;
+//import com.sellect.server.order.Infrastructure.port.FakePayClient;
+//import com.sellect.server.order.Infrastructure.response.KakaoPayReadyResponse;
+//import com.sellect.server.order.application.OrderService;
+//import com.sellect.server.order.application.v1.OrderServiceV1After;
+//import com.sellect.server.order.controller.request.OrderAddRequest;
+//import com.sellect.server.order.controller.response.PendingOrderRegisterResponse;
+//import jakarta.validation.Valid;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//
+///***
+// * Order V1 부하 테스트용 컨트롤러
+// * */
+//@RequiredArgsConstructor
+//@Slf4j
+//@RequestMapping("/api/v1/test")
+//@RestController
+//public class OrderV1ScenarioTestController {
+//
+//    private final OrderServiceV1After orderService;
+//    private final OrderService orignalOrderService;
+//
+//    /**
+//     * 주문 생성(pending)
+//     */
+//    @PostMapping("/order/pending/{userId}")
+//    public ApiResponse<PendingOrderRegisterResponse> registerPendingOrder(@PathVariable Long userId,
+//        @Valid @RequestBody OrderAddRequest requests) {
+//        User user = User.builder()
+//            .id(userId)
+//            .build();
+//        PendingOrderRegisterResponse response = orignalOrderService.registerPendingOrder(user,
+//            requests);
+//        return ApiResponse.ok(response);
+//    }
+//
+//
+//    /***
+//     * 결제요청 테스트용 api
+//     * */
+//    @PostMapping("/order/payment/{orderId}/ready/{userId}")
+//    public ApiResponse<String> readyPaymentTest(@PathVariable String orderId,
+//        @PathVariable Long userId) {
+//
+//        log.info("[V1] ready!!");
+//        User user = User.builder()
+//            .id(userId)
+//            .build();
+//
+//        KakaoPayReadyResponse kakaoPayReadyResponse = orderService.preparePayment(user,
+//            Long.valueOf(orderId));
+//        // [TODO: 성능 테스트를 위해서 어쩔 수 없이 추가했어야 함. 배포시 삭제해야함]
+//        FakePayClient.triggerInProgress(kakaoPayReadyResponse.tid(), kakaoPayReadyResponse.next_redirect_pc_url());
+//        return ApiResponse.ok(kakaoPayReadyResponse.next_redirect_pc_url());
+////        String pid = orderService.preparePayment(user, orderId);
+////        return ApiResponse.ok(pid);
+//    }
+//
+//
+//    /***
+//     * 결제승인 테스트용 API
+//     * */
+//    @GetMapping("/kakao-pay/success/{pid}")
+//    public ApiResponse<String> approvePayment(
+//        @PathVariable String pid,
+//        @RequestParam("pg_token") String token) {
+//
+//        long threadId = Thread.currentThread().getId();
+//        log.info("{} - [V1] approve", threadId);
+//        orderService.approvePayment(Long.valueOf(pid), token);
+//        log.info("{} - [V1] success", threadId);
+//        return ApiResponse.ok(pid + "success");
+//    }
+//}
