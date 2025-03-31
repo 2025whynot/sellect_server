c3768261b04c0797828b3034cc9e17df51a36dd5
Sangkwon Lee <sangkwon26@gmail.com>
2025-03-31 15:50:26 +0900

    update: 성능테스트 - 방법 1. 데드락에 대한 회피 방식 (정렬) -> 기아현상 발생

--- src/main/java/com/sellect/server/order/application/v1/approvepayment/ApprovePaymentConfig.java
+++ src/main/java/com/sellect/server/order/application/v1/approvepayment/ApprovePaymentConfig.java
@@ -6,15 +6,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ApprovePaymentConfig {
 
-    @Bean
-    public ApprovePaymentStrategy approvePaymentStrategy(ApprovePaymentV0 v0) {
-        return v0; // 기존 코드: 데드락에 대한 아무런 조치가 없음
-    }
-//
 //    @Bean
-//    public ApprovePaymentStrategy approvePaymentStrategy(ApprovePaymentV1 v1) {
-//        return v1; // 방법 1. 데드락에 대한 회피 방식 (정렬) -> 기아현상 발생
+//    public ApprovePaymentStrategy approvePaymentStrategy(ApprovePaymentV0 v0) {
+//        return v0; // 기존 코드: 데드락에 대한 아무런 조치가 없음
 //    }
+//
+    @Bean
+    public ApprovePaymentStrategy approvePaymentStrategy(ApprovePaymentV1 v1) {
+        return v1; // 방법 1. 데드락에 대한 회피 방식 (정렬) -> 기아현상 발생
+    }
 //
 //    @Bean
 //    public ApprovePaymentStrategy approvePaymentStrategy(ApprovePaymentV2 v2) {
